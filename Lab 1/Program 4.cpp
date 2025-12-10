3c507266a0159595999a9a26c66a0ff345c73e0c
AbdullahUsmani123 <usmaniabdullah98@gmail.com>
2025-12-11 00:29:45 +0500

    Add files via upload

---
+++ Lab 1/Program 4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"She said, \"Lets learn C++!\"\n";
+    cout<<"To start save your file in C\\C_Project\\Lab1\n";
+    cout<<"Good Luck!";
+    return 0;
+}
